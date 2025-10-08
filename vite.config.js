4b5f817a3534f4d1bf6126ddc6e00ba3ea577f65
SarVeshvarn R <itzzsvrtech@SarVeshvarns-MacBook-Air.local>
2025-10-08 20:18:55 +0530

    Deployment issues fixed

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    base: "/yt-portfolio",
 });
